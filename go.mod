a8e19fb9b2e99b96ea3c9c0014889d223915d322
Andrew Roe <andrewroe34@yahoo.com>
2024-12-10 21:02:21 -0500

    feat: Initial Go backend server skeleton

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/Sourceware-Lab/backend-proto
+
+go 1.22.2
---
+++ main.go
@@ -0,0 +1,24 @@
+package main
+
+import (
+	"log"
+	"net/http"
+)
+
+// Define a home handler function which writes a byte slice containing
+// "Hello from RealQuick" as the response body.
+func home(w http.ResponseWriter, r *http.Request) {
+	w.Write([]byte("Hello from RealQuick"))
+}
+
+func main() {
+	// Register handler function and corresponding route pattern with the servemux
+	mux := http.NewServeMux()
+	mux.HandleFunc("/", home)
+
+	log.Print("starting server on :4000")
+
+	// check via: http://localhost:4000/
+	err := http.ListenAndServe(":4000", mux)
+	log.Fatal(err)
+}
