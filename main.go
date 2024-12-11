6a183ff937e1342d7280c08ead61b5300d979155
Peter <peterHoburg@users.noreply.github.com>
2024-12-11 07:50:19 -0500

    Moved to using CLI

--- main.go
+++ main.go
@@ -1,18 +1,25 @@
 package main
 
 import (
+	"fmt"
 	"net/http"
 
 	"github.com/danielgtaylor/huma/v2"
 	"github.com/danielgtaylor/huma/v2/adapters/humagin"
 	_ "github.com/danielgtaylor/huma/v2/formats/cbor"
+	"github.com/danielgtaylor/huma/v2/humacli"
 	"github.com/gin-gonic/gin"
 
 	"github.com/Sourceware-Lab/backend-proto/api/greeting"
 )
 
-// GreetingOutput represents the greeting operation response.
+const apiVersion = "0.0.1"
 
+type Options struct {
+	Port int `help:"Port to listen on" short:"p" default:"8888"`
+}
+
+// This is to make testing easier. We can pass a testing API interface.
 func addRoutes(api huma.API) {
 	// Register GET /greeting/{name}
 	huma.Register(api, huma.Operation{
@@ -39,12 +46,25 @@ func addRoutes(api huma.API) {
 	)
 }
 
+func getCli() (cli humacli.CLI) { // this -> (cli humacli.CLI) is a really cool go feature. It inits the var, and
+	// when you use a raw return it will return the var called cli. This improves the go auto docs.
+	cli = humacli.New(func(hooks humacli.Hooks, options *Options) {
+		// Create a new router & API
+		router := gin.New()
+		api := humagin.New(router, huma.DefaultConfig("Example API", apiVersion))
+
+		addRoutes(api)
+
+		// Tell the CLI how to start your server.
+		hooks.OnStart(func() {
+			fmt.Printf("Starting server on port %d...\n", options.Port)
+			_ = http.ListenAndServe(fmt.Sprintf(":%d", options.Port), router)
+		})
+	})
+	return
+}
+
 func main() {
-	// Create a new router & API
-	router := gin.New()
-	api := humagin.New(router, huma.DefaultConfig("My API", "0.0.1"))
-
-	addRoutes(api)
-
-	http.ListenAndServe("0.0.0.0:4000", router)
+	cli := getCli()
+	cli.Run()
 }
