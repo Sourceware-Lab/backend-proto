23e39d8b63cad9f123fbbe9b345b0922f0540638
Peter <peterHoburg@users.noreply.github.com>
2024-12-11 07:41:16 -0500

    Moved funcs into package, Moved api registration into function, added HORRIBLE post example

---
+++ api/greeting/handler.go
@@ -0,0 +1,19 @@
+package greeting
+
+import (
+	"context"
+	"fmt"
+)
+
+func Get(ctx context.Context, input *Input) (*Output, error) {
+	resp := &Output{}
+	resp.Body.Message = fmt.Sprintf("Hello get , %s!", input.Name)
+	return resp, nil
+}
+
+func Post(ctx context.Context, input *Input) (*Output, error) {
+	resp := &Output{}
+	resp.Body.Message = fmt.Sprintf("Hello post , %s!", input.Name)
+	return resp, nil
+
+}
--- main.go
+++ main.go
@@ -1,36 +1,50 @@
 package main
 
 import (
-	"context"
-	"fmt"
 	"net/http"
 
 	"github.com/danielgtaylor/huma/v2"
 	"github.com/danielgtaylor/huma/v2/adapters/humagin"
 	_ "github.com/danielgtaylor/huma/v2/formats/cbor"
 	"github.com/gin-gonic/gin"
+
+	"github.com/Sourceware-Lab/backend-proto/api/greeting"
 )
 
 // GreetingOutput represents the greeting operation response.
-type GreetingOutput struct {
-	Body struct {
-		Message string `json:"message" example:"Hello, world!" doc:"Greeting message"`
-	}
-}
 
-type GreetingInput struct {
-	Name string `path:"name" maxLength:"30" example:"world" doc:"Name to greet"`
+func addRoutes(api huma.API) {
+	// Register GET /greeting/{name}
+	huma.Register(api, huma.Operation{
+		OperationID: "get-greeting",
+		Method:      http.MethodGet,
+		Path:        "/greeting/{name}",
+		Summary:     "Get a greeting",
+		Description: "Get a greeting for a person by name.",
+		Tags:        []string{"Greetings"},
+	},
+		greeting.Get,
+	)
+
+	// Register POST /reviews
+	huma.Register(api, huma.Operation{
+		OperationID:   "post-greeting",
+		Method:        http.MethodPost,
+		Path:          "/greeting",
+		Summary:       "Post a greeting",
+		Tags:          []string{"Greetings"},
+		DefaultStatus: http.StatusCreated,
+	},
+		greeting.Post,
+	)
 }
 
 func main() {
 	// Create a new router & API
 	router := gin.New()
-	api := humagin.New(router, huma.DefaultConfig("My API", "1.0.0"))
+	api := humagin.New(router, huma.DefaultConfig("My API", "0.0.1"))
 
-	huma.Get(api, "/greeting/{name}", func(ctx context.Context, input *GreetingInput) (*GreetingOutput, error) {
-		resp := &GreetingOutput{}
-		resp.Body.Message = fmt.Sprintf("Hello, %s!", input.Name)
-		return resp, nil
-	})
+	addRoutes(api)
+
 	http.ListenAndServe("0.0.0.0:4000", router)
 }
